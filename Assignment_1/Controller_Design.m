c1eaf539a83f321cf4d28368863fa5fe830ed786
ebtls <zjx119009@163.com>
2022-03-08 15:18:45 +0100

    add mlx

--- Assignment_1/Controller_Design.m
+++ Assignment_1/Controller_Design.m
@@ -7,7 +7,8 @@
 %% Parameters
 % Dual Rotary System Parameters
 DR.Km = 4.4 * 1e-2;
-DR.J1 = 3.75 * 1e-6; DR.J2 = 3.75 * 1e-6;
+DR.J1 = 3.75 * 1e-6; 
+DR.J2 = 3.75 * 1e-6;
 DR.b = 1 * 1e-5;
 DR.k = 0.2656;
 DR.d = 3.125 * 1e-5;
@@ -24,9 +25,9 @@ DCM.nx = 2;
 DCM.nu = 1;
 
 % sampling time(s)
-DR.h = 0.2e-3;
+DR.h = 4e-3;
 DCM.h = 2 * DR.h;
-tau = 0.3e-3;
+tau = 4e-4;
 
 %% State Space Model
 % Dual Rotary System
@@ -54,18 +55,23 @@ DR_CS.sysc = ss(DR_CS.A, DR_CS.B, DR_CS.C, 0);
 
 % to discrete
 DR_CS.sysd = c2d(DR_CS.sysc, DR.h);
-DR_CS.phi = DR_CS.sysd.a; DR_CS.Gamma = DR_CS.sysd.b; DR_CS.Cd = DR_CS.sysd.c;
+DR_CS.phi = DR_CS.sysd.a; 
+DR_CS.Gamma = DR_CS.sysd.b; 
+DR_CS.Cd = DR_CS.sysd.c;
+
 % Desired closed-loop poles and pole placement
-DR_CS.alpha = [0.95 0.98 0.98 0.98];
-% feedback vector
+DR_CS.alpha = [0.1 0.1 0.1 0.1];
+
+% feedback vector5
 DR_CS.K = -acker(DR_CS.phi, DR_CS.Gamma, DR_CS.alpha);
+
 % feedforward gain
 temp = inv(eye(4) - DR_CS.phi - DR_CS.Gamma * DR_CS.K) * DR_CS.Gamma;
-DR_CS.F = 1 / (DR_CS.C * temp);
+DR_CS.F = 1 / (DR_CS.Cd * temp);
 
-DR_CS.full_sysc = ss(DR_CS.A + DR_CS.B * DR_CS.K, DR_CS.B * DR_CS.F, DR_CS.C, 0);
-DR_CS.full_sysd = ss(DR_CS.full_sysc, DR.h);
-DR_CS.full_tf = tf(DR_CS.full_sysd);
+% DR_CS.full_sysc = ss(DR_CS.phi + DR_CS.Gamma * DR_CS.K, DR_CS.Gamma * DR_CS.F, DR_CS.Cd, 0);
+% DR_CS.full_sysd = c2d(DR_CS.full_sysc, DR.h);
+% DR_CS.full_tf = tf(DR_CS.full_sysd);
 
 % DCM Control System
 % continuous-time
@@ -73,32 +79,25 @@ DCM_CS.sysc = ss(DCM_CS.A, DCM_CS.B, DCM_CS.C, 0);
 
 % to discrete
 DCM_CS.sysd = c2d(DCM_CS.sysc, DCM.h);
-DCM_CS.phi = DCM_CS.sysd.a; DCM_CS.Gamma = DCM_CS.sysd.b; DCM_CS.Cd = DCM_CS.sysd.c;
+DCM_CS.phi = DCM_CS.sysd.a; 
+DCM_CS.Gamma = DCM_CS.sysd.b; 
+DCM_CS.Cd = DCM_CS.sysd.c;
 
 % Desired closed-loop poles and pole placement
-DCM_CS.alpha = [0.998 0.998];
-
+DCM_CS.alpha = [0.9 0.9];
 % feedback vector
 DCM_CS.K = -acker(DCM_CS.phi, DCM_CS.Gamma, DCM_CS.alpha);
 
 % feedforward gain
 temp = inv(eye(2) - DCM_CS.phi - DCM_CS.Gamma * DCM_CS.K) * DCM_CS.Gamma;
-DCM_CS.F = 1 / (DCM_CS.C * temp);
+DCM_CS.F = 1 / (DCM_CS.Cd * temp);
 
-DCM_CS.full_sysc = ss(DCM_CS.A + DCM_CS.B * DCM_CS.K, DCM_CS.B * DCM_CS.F, DCM_CS.C, 0);
-DCM_CS.full_sysd = ss(DCM_CS.full_sysc, DCM.h);
-DCM_CS.full_tf = tf(DCM_CS.full_sysd);
+% DCM_CS.full_sysc = ss(DCM_CS.phi + DCM_CS.Gamma * DCM_CS.K, DCM_CS.Gamma * DCM_CS.F, DCM_CS.Cd, 0);
+% DCM_CS.full_sysd = c2d(DCM_CS.full_sysc, DCM.h);
+% DCM_CS.full_tf = tf(DCM_CS.full_sysd);
 
-% Review Controller Result
-figure
-DR_CS.S=stepinfo(DR_CS.full_tf);
-step(DR_CS.full_tf);
-grid on;
-title("DR CS")
-figure
-DCM_CS.S=stepinfo(DCM_CS.full_tf);
-step(DCM_CS.full_tf);
-grid on;
-title("DCM CS")
+% Simulink Simulation
+assignment1_2022_Simulink_init_Dualrotary(tau,DR.h,DR_CS.K, DR_CS.F)
+% assignment1_2022_Simulink_init_DCmotor(0,DCM.h,DCM_CS.K,DCM_CS.F)
 
 
