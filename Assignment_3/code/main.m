f9a33fd6f424e5afd7343253e680c9a4a9dd56c7
kemosabe564 <liyiting.kemos@gmail.com>
2022-04-13 00:13:45 +0200

    update 4.13

--- Assignment_3/code/main.m
+++ Assignment_3/code/main.m
@@ -3,14 +3,23 @@
 %%% Assignment 3
 %%%%%
 
+close all
+clear all
+clc
 
 %% load resources
 addpath('./src')
 
-%% configuration 
-case_id=1; % 1 for CAN, 2 for FlexRay
-
 %% Gloabl Parameter
+case_id = 2; % 1 for CAN, 2 for FlexRay
+EMB.fh = 1/60;
+
+%simulation
+Initial_value = 0;
+Simulation_time = 20;
+PATTERN = {1};
+Reference = 0.002;
+
 %% State Space Model
 % Emergency Braking System
 EMB_CS.nx = 5;
@@ -33,20 +42,15 @@ EMB_CS.D = 0;
 
 %% CAN case
 if case_id==1
-    
-        %% Initialization
-    Reference = 0.002;
+% Initialization
     % sampling time(s) & delay
-    EMB.fh = 1/60;
     EMB.n_pipeline = 1;
     EMB.tau = 133.33e-3;
     EMB.h = 133.33e-3;
     EMB.contorller_type = 1;
-    Initial_value = 0;
-    Simulation_time = 20;
 
 
-    %%
+
     % agumentation
     if (EMB.n_pipeline <= 1)
         EMB_CS = augmentSystem(EMB.h, EMB.h, EMB.n_pipeline, EMB_CS);
@@ -55,71 +59,41 @@ if case_id==1
     end
     EMB_CS.R = 1;
 
-    EMB_CS.Q = 10*eye(size(EMB_CS.C_aug{1},2),size(EMB_CS.C_aug{1},2));
-    EMB_CS.Q(5,5)=100;
+    EMB_CS.Q = 3 * eye(size(EMB_CS.C_aug{1}, 2), size(EMB_CS.C_aug{1}, 2));
+    EMB_CS.Q(5,5) = 1e10;
     [EMB_CS.K, EMB_CS.F, EMB_CS.K_T, EMB_CS.T] = discreteTimeController(EMB.tau, EMB.h, EMB_CS.Q, EMB_CS.R, EMB_CS);
-
-
-    % Simulation
-    CONTROLLER_TYPE = 1;
-    SIMULATION_TIME = 10;
-    Reference= 0.002;
-    PATTERN = {1};
-    
-    if EMB.n_pipeline <= 1
-        simulateDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, PATTERN, Simulation_time, Reference, EMB.fh);
-    else
-        simulatePipelinedDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, Simulation_time, Reference)
-    end
-
-    clear PATTERN
-
-
-
 
 %% FlexRay Case
 elseif case_id==2
-    
-    %% Initialization
-    Reference = 0.002;
+% Initialization
     % sampling time(s) & delay
-    EMB.fh = 1/60;
     EMB.n_pipeline = 1;
-    EMB.n_pipeline = 5;
-    EMB.tau = 90e-3;
-    EMB.h = 90e-3;
+    EMB.tau = 126e-3;
+    EMB.h = 126e-3;
     EMB.contorller_type = 1;
-    Initial_value = 0;
-    Simulation_time = 20;
 
-
-    %%
-    % agumentation
+    %agumentation
     if (EMB.n_pipeline <= 1)
         EMB_CS = augmentSystem(EMB.h, EMB.h, EMB.n_pipeline, EMB_CS);
     else
         EMB_CS = augmentSystem(EMB.tau, EMB.h, EMB.n_pipeline, EMB_CS);
     end
     EMB_CS.R = 1;
+    
+    temp = [0 1 100 2 1 0];
 
-    temp = [0.76 0.22 0.61 0.015 0 0 0 0 ];
-    temp = [0 1 1 2 9 0];
-
-
-    EMB_CS.Q = 100*(temp') * temp;
+    EMB_CS.Q = 100000*(temp') * temp;
     clear temp
     [EMB_CS.K, EMB_CS.F, EMB_CS.K_T, EMB_CS.T] = discreteTimeController(EMB.tau, EMB.h, EMB_CS.Q, EMB_CS.R, EMB_CS);
 
 
-    %% Simulation
-
-    if EMB.n_pipeline <= 1
-        simulateDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, PATTERN, Simulation_time, Reference, EMB.fh);
-    else
-        simulatePipelinedDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, Simulation_time, Reference)
-    end
-
-    clear PATTERN
-
 end
 
+
+if EMB.n_pipeline <= 1
+    simulateDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, PATTERN, Simulation_time, Reference, EMB.fh);
+else
+    simulatePipelinedDIC(Initial_value, EMB.contorller_type, EMB.h, EMB.tau, EMB_CS.Phi_aug, EMB_CS.Gamma_aug, EMB_CS.C_aug, EMB_CS.K, EMB_CS.F, Simulation_time, Reference)
+end
+
+clear PATTERN
